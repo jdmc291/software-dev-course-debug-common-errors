0d3c07cbdb5d2b4fb8685e8c5165ec5f629a733a
Jalen McBride <jamcbride131@gmail.com>
2025-03-23 11:44:14 -0500

    I corrected the following statements

--- debug-common-errors.js
+++ debug-common-errors.js
@@ -24,23 +24,23 @@ Think about which debugging methods you found most useful and how you might appl
 // Description:
 // This program is intended to display a simple prompt in the console but fails to run.
 
-console.log("Welcome to the bootcamp
+console.log("Welcome to the bootcamp");
 
 // What’s Wrong?
-
+//My answer: It is missing a closed parenthesis, and closed double quotation, and a semicolon
 
 // Program B
 // Description:
 // This code attempts to multiply each number in an array by 2 and display the results. However, it crashes at runtime.
 
-let numbers = [2, 4, "eight"];
+let numbers = [2, 4, 8];
 for (let i = 0; i < numbers.length; i++) {
   let doubled = numbers[i] * 2;
   console.log(doubled);
 }
 
 // What’s Wrong?
-
+//MY Answer: the string 8 cannot be multipied by 2. It would indicate that is is NaN (not a number)
 
 
 // Program C (Logic Error)
@@ -51,12 +51,13 @@ function isPrime(num) {
   if (num < 2) return false;
   for (let i = 2; i < num; i++) {
     if (num % i === 0) {
-      return true;  // Supposed to indicate num is NOT prime
+      return false;  // Supposed to indicate num is NOT prime
     }
   }
-  return false; // Supposed to indicate num IS prime
+  return true; // Supposed to indicate num IS prime
 }
 
 console.log(isPrime(7)); // Expected true but gets false
 
 // What’s Wrong?
+//MY Answer: Line 54 and 60 needed to be swapped. If any of the for loop statements show modulus 0 is true, it should indicate that the number entered is NOT Prime
